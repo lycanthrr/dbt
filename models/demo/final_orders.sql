0fc4e2a612bc50fb3bbcd40d276ce02184e06103
Dallin Ozaki <dallinmozaki@gmail.com>
2026-07-30 01:04:58 +0000

    ephemeral

---
+++ models/demo/final_orders.sql
@@ -0,0 +1,17 @@
+{{
+    config
+    (
+        materialized = 'table'
+    )
+}}
+with clean_orders as (
+    select
+        ORDER_ID,
+        ORDER_DATE,
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        CREATED_AT,
+        CURRENT_TIMESTAMP AS INSERT_DTS,
+    from {{ ref('clean_orders') }}
+)
+select * from clean_orders
